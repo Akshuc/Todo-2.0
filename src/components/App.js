42b0a9267e522014363a886d7cc0266ae7f45df5
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-23 05:21:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import SideNav from './SideNav';
 import MainSection from './MainSection';
 import Header from './Header';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/App.css';
 const App = () => {
-  const [state, setState] = useState("INBOX");
-  function kroChange(inp){
-     setState(inp);
-  }
+
+
   return (
     <div id="main">
     <Header />
       <div className='row'>
         <div className='col-md-2'>
-          <SideNav change={kroChange}/>
+          <SideNav change={}/>
         </div>
         <div className='col-md-10'>
-          <MainSection active = {state}/>
+          <MainSection active = {}/>
         </div>
       </div>
     </div>
